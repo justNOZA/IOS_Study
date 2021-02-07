25613ea9a440b0abe0f0f317c57a88921dc63129
pasonatech <nagoyalab@pasona.tech>
2021-02-08 00:19:29 +0900

    ㅅㅂ

--- FirstPractice/FirstPractice/ViewController.swift
+++ FirstPractice/FirstPractice/ViewController.swift
@@ -12,8 +12,44 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        Greet.text = NSLocalizedString("Labelname", comment: "")
+        
+
     }
 
-
+    @IBAction func ClickChangeBtn(_ sender: Any) {
+        
+    }
+    
+    @IBOutlet weak var Greet: UILabel!
+    
+}
+/*
+extension UILabel{
+    @IBInspectable
+    private var locallizedKey:String?{
+        get {
+            fatalError("Only set this value")
+        }
+        set{
+            if let newValue = newValue{
+                text = newValue.localized()
+                
+            }
+        }
+    }
 }
 
+extension String {
+    private static let localizedEmptyKey = "##not exists##"
+    func localized() -> String {
+        let string = NSLocalizedString(self, tableName: nil, bundle: Bundle.main, value: String.localizedEmptyKey, comment: "")
+        if string == String.localizedEmptyKey {
+            fatalError("not exists localized key")
+        }
+        return string
+    }
+
+}
+*/
