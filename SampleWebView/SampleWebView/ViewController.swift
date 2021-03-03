7572b382a0e7ab41d0a99493f2eb04697655265c
justNOZA <1995pjs2635@gmail.com>
2021-03-03 23:23:02 +0900

    testing

--- SampleWebView/SampleWebView/ViewController.swift
+++ SampleWebView/SampleWebView/ViewController.swift
@@ -25,12 +25,12 @@ class ViewController: UIViewController {
             let urlReq = URLRequest(url: url)
             WebViewMain.load(urlReq)
         }
-        var abc = "1"
-        var abc1 : String? = nil
-        
-        var aaa = abc1! //값이 무조건 있을 경우 느낌표로 전환한 것만으로 언랩핑이 가능하다. nil값일 때 강제로 언랩핑할 경우 오류 발생! 주의
-        if let bbb = abc1 {}
-        let abc2 = "2"
+//        var abc = "1"
+//        var abc1 : String? = nil
+//        
+//        var aaa = abc1! //값이 무조건 있을 경우 느낌표로 전환한 것만으로 언랩핑이 가능하다. nil값일 때 강제로 언랩핑할 경우 오류 발생! 주의
+//        if let bbb = abc1 {}
+//        let abc2 = "2"
         //let의 경우에는 상수이기 때문에 옵셔널을 쓰는 것이 애초에 불필요한 과정이다.
         
         //null pointer exception - java의 경우
