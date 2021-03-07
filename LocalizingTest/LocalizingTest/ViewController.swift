2ff8119f2115cebfba86ddebef4baa230a899d59
justNOZA <1995pjs2635@gmail.com>
2021-03-07 15:44:35 +0900

    localizing

--- LocalizingTest/LocalizingTest/ViewController.swift
+++ LocalizingTest/LocalizingTest/ViewController.swift
@@ -9,11 +9,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var helloLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+//        helloLabel.text = NSLocalizedString("Hello", comment: "")
     }
 
 
 }
 
+extension UILabel {
+    @IBInspectable
+    private var localizedKey: String? {
+        get { fatalError("only set this value") }
+        set {
+            if let newValue = newValue {
+                text = newValue.localized()
+            }
+        }
+    }
+}
+
+extension String {
+    func localized() -> String? {
+        return NSLocalizedString(self, comment: "")
+    }
+}
