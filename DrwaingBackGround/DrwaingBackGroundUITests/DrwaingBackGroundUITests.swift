5ce5db555a1f8108caeaa6f287781ebc416b68d1
justNOZA <1995pjs2635@gmail.com>
2021-04-12 19:06:54 +0900

    app launch

--- DrwaingBackGround/DrwaingBackGroundUITests/DrwaingBackGroundUITests.swift
+++ DrwaingBackGround/DrwaingBackGroundUITests/DrwaingBackGroundUITests.swift
@@ -6,12 +6,19 @@ class TestSign: XCTestCase {
     
     override func setUp() {
         super.setUp()
+        deleteAllData()
     }
     
     // No.01-1
     func testScreenFactor(){
         let canvasElement = app.otherElements["canvas"]
         canvasElement.swipeRight()
+    }
+    
+    func deleteAllData() {
+        let app = XCUIApplication()
+        app.launchArguments = ["--Reset"]
+        app.launch()
     }
 
 }
