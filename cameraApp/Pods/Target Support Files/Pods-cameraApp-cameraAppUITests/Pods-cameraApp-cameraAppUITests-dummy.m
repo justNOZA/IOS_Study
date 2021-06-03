e9a315623e7e77282ecb10db2e720d8100d78baf
justNOZA <1995pjs2635@gmail.com>
2021-06-03 09:56:57 +0900

    no message

--- cameraApp/Pods/Target Support Files/Pods-cameraApp-cameraAppUITests/Pods-cameraApp-cameraAppUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_cameraApp_cameraAppUITests : NSObject
-@end
-@implementation PodsDummy_Pods_cameraApp_cameraAppUITests
-@end
--- cameraApp/Pods/Target Support Files/Pods-cameraApp/Pods-cameraApp-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_cameraApp : NSObject
-@end
-@implementation PodsDummy_Pods_cameraApp
-@end
